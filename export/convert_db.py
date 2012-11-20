0176af06d7182959169151c69ac575c6d734ed28
Bartosz Telenczuk <muchatel@poczta.fm>
2012-11-20 15:44:04 +0100

    added psth height to cell table

--- export/convert_db.py
+++ export/convert_db.py
@@ -8,7 +8,7 @@ from spike_sort.io.filters import BakerlabFilter
 from tables.exceptions import NoSuchNodeError
 import os
 import patterns
-from matplotlib.mlab import rec2csv
+from matplotlib.mlab import rec2csv, csv2rec
 
 
 #cell_db = '../cell_bwoser/src/cell_db.h5'
@@ -23,36 +23,39 @@ if __name__ == "__main__":
     
     records = []
     h5f = tables.openFile(cell_db)
+    cell_csv = csv2rec(DATAPATH+'cell_db.csv')
     data_filter = BakerlabFilter(DATAPATH+'gollum_export.inf')
 
-    for node in h5f.walkNodes():
-        if 'cell' in node._v_name:
-            cell_path = "/".join(node._v_pathname.split('/'))
-            stim_path = "/".join(node._v_pathname.split('/')[:-1]+['stim'])
-            try:
-                events = node.cache.events.read()
-                n_events = len(events)
-            except NoSuchNodeError:
-                n_events = 1
-            
-            try:
-                score = int(node.cache.score[0])
-            except NoSuchNodeError:
-                score = 0
+    for cellid in cell_csv['id']:
+        node = h5f.getNode('/', cellid) 
+        cell_path = "/".join(cellid.split('/'))
+        stim_path = "/".join(cellid.split('/')[:-1]+['stim'])
+        try:
+            events = node.cache.events.read()
+            n_events = len(events)-1
+        except NoSuchNodeError:
+            n_events = 1
+        
+        try:
+            score = int(node.cache.score[0])
+        except NoSuchNodeError:
+            score = 0
 
-            spt = data_filter.read_spt(cell_path)
-            stim = data_filter.read_spt(stim_path)
-            n_spikes = len(spt['data'])
-            n_trials = len(stim['data'])
-            try:
-                psth, psth_time = patterns.CalcPSTH(spt['data'], stim['data'])
-                psth_peak = psth.max()
-            except IndexError:
-                psth_peak = np.NaN
-            stim_start = 0
-            stim_stop = -1
-            records.append((cell_path, n_events, n_spikes, n_trials,
-                            psth_peak, stim_start, stim_stop, score))
+        spt = data_filter.read_spt(cell_path)
+        stim = data_filter.read_spt(stim_path)
+        n_spikes = len(spt['data'])
+        n_trials = len(stim['data'])
+        try:
+            psth, psth_time = patterns.CalcPSTH(spt['data'],
+                                                stim['data'],
+                                                norm=True)
+            psth_peak = psth.max()
+        except IndexError:
+            psth_peak = np.NaN
+        stim_start = 0
+        stim_stop = -1
+        records.append((cell_path, n_events, n_spikes, n_trials,
+                        psth_peak, stim_start, stim_stop, score))
 
     rec_array = np.rec.fromrecords(records, 
                     names=('cell,n_events,n_spikes,n_trials,psth_height,'
